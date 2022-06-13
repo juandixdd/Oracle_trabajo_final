7654cafd4646c9f140df1651bc63e542e7021634
Juandi <juandixarteagaperez210@gmail.com>
2022-06-13 18:41:53 -0500

    Se termina parte 1

--- parte_1.sql
+++ parte_1.sql
@@ -17,3 +17,27 @@ create table CIUDADES(
     Poblacion INT,
     IDDiv INT
 );
+
+create table DIVISIONES(
+    IDDiv INT,
+    NomDiv VARCHAR(30),
+    Extensión INT,
+    IDPais INT
+);
+
+create table PAISES(
+    IDPais INT,
+    NomPais VARCHAR(30),
+    IDIdioma INT,
+    IDTipo INT
+);
+
+create table IDIOMAS(
+    IDIdioma INT,
+    NombIdioma VARCHAR(30)
+);
+
+create table TIPODIVISION(
+    IDTipo INT,
+    NombTipo VARCHAR(30)
+);
