a436b9e9df893ea768247581add3566479bfb4fe
Juandi <juandixarteagaperez210@gmail.com>
2022-06-13 18:36:06 -0500

    Se agregan dos tablas

--- parte_1.sql
+++ parte_1.sql
@@ -1,2 +1,19 @@
 /* Oracle Querys parte 1 */
 
+create table PERSONAS(
+    IDPer INT,
+    NomPer VARCHAR(30),
+    ApePer VARCHAR(30),
+    Direccion VARCHAR(40),
+    Telefono VARCHAR(10),
+    LugRes INT,
+    FecNac DATE,
+    LugNac INT
+);
+
+create table CIUDADES(
+    IDCiu INT,
+    NomCiu VARCHAR(30),
+    Poblacion INT,
+    IDDiv INT
+);
